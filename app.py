a782849941065c4ef78c3bdba71eadeb85c2b16c
SSpectre <jordan.barron.hurst@gmail.com>
2024-03-31 03:42:46 -0400

    Implement OpenID and basic Flask structure; get user's game list from Steam API

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, redirect, request, url_for
+from urllib.parse import urlencode
+
+from steam_user import SteamUser
+
+app = Flask(__name__)
+app.debug = True
+
+steam_openid_url = 'https://steamcommunity.com/openid/login'
+
+user: SteamUser
+
+@app.route("/")
+def hello():
+    return '<a href="/login">Login with steam</a>'
+
+@app.route("/login")
+def login_with_steam():
+    params = {
+      'openid.ns': "http://specs.openid.net/auth/2.0",
+      'openid.identity': "http://specs.openid.net/auth/2.0/identifier_select",
+      'openid.claimed_id': "http://specs.openid.net/auth/2.0/identifier_select",
+      'openid.mode': 'checkid_setup',
+      'openid.return_to': 'http://127.0.0.1:5000/user',
+      'openid.realm': 'http://127.0.0.1:5000'
+      }
+  
+    query_string = urlencode(params)
+    login_url = steam_openid_url + "?" + query_string
+    print(login_url)
+    return redirect(login_url)
+
+@app.route("/user")
+def get_user_id():
+    identity = request.args["openid.identity"]
+    last_slash = identity.rindex('/')
+    id_number = identity[last_slash+1:]
+    
+    global user
+    user = SteamUser(id_number)
+    return redirect(url_for("list_owned_games", user_id = user.user_id))
+
+@app.route("/user/<user_id>")
+def list_owned_games(user_id):
+    #if user tries to bypass login by directly entering Steam id, exception is thrown
+    try:
+        return "Logged in as " + user.user_name
+    except Exception:
+        return '<a href="/login">Login with steam</a>'
+
+if __name__ == "__main__":
+    app.run()
